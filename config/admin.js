e0dc5f9217c65e3482bf1b9b0b6678270559fbaa
yqssirkensouss <y.kensouss@qalqul.io>
2022-08-02 16:25:08 -0500

    updates

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET','muwWBzyVNQkmCrwU80q24g=='),
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT','jG2IVYPoyKpBfBBHBojgVg=='),
+    salt: env('API_TOKEN_SALT'),
   },
 });
