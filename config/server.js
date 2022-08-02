120c723caed1008392ec0dc9a08146999f8a7e74
yqssirkensouss <y.kensouss@qalqul.io>
2022-08-02 16:12:33 -0500

    updates

--- config/server.js
+++ config/server.js
@@ -1,17 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array("APP_KEYS", [
-      "HOST",
-      "PORT",
-      "APP_KEYS",
-      "API_TOKEN_SALT",
-      "ADMIN_JWT_SECRET",
-      "JWT_SECRET",
-      "CLOUDINARY_NAME",
-      "CLOUDINARY_KEY",
-      "CLOUDINARY_SECRET"
-    ]),
+    keys: env.array('APP_KEYS'),
   },
 });
